4e4c0e863d4b1bac8811b5aeb7d24254a843865f
Ryan J Yoder <ryanathome@Ryans-MacBook-Pro.local>
2018-07-15 22:39:43 -0700

    shell

--- cmd/domainproxy/domainproxy.go
+++ cmd/domainproxy/domainproxy.go
@@ -24,6 +24,6 @@ func addDomains(mux *domainproxy.DomainMux) {
 	tekDomain := "tekcitadel.com"
 	mux.SetHandler(tekDomain, tekProxy)
 
-	url, _ := url.Parse("http://10.247.165.131:38941")
+	url, _ := url.Parse("http://10.111.74.111:8080")
 	mux.SetHandler("shell.ryanjyoder.com", httputil.NewSingleHostReverseProxy(url))
 }
