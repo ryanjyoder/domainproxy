77f0d08c18baf66ff38ddde4142ff540d7aa9414
Ryan J Yoder <ryanathome@Ryans-MacBook-Pro.local>
2018-07-15 20:53:11 -0700

    bind to any ip

--- cmd/testserver/testserver.go
+++ cmd/testserver/testserver.go
@@ -3,18 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"time"
 )
 
 func main() {
 	message := "Welcome to the test server!"
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, message)
 		//fmt.Println(sURL, "path:", r.URL.String())
 	}))
-	defer s.Close()
 
-	fmt.Println("URL:", s.URL)
 	time.Sleep(1 * time.Hour)
 }
