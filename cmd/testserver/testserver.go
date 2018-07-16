ece4feb3ab027c7750184dc7d61bf1b5de669808
Ryan J Yoder <ryanathome@Ryans-MacBook-Pro.local>
2018-07-15 21:00:08 -0700

    bind to any ip

--- cmd/testserver/testserver.go
+++ cmd/testserver/testserver.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	message := "Welcome to the test server!"
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("remote:", r.RemoteAddr)
 		fmt.Fprintln(w, message)
 		//fmt.Println(sURL, "path:", r.URL.String())
 	}))
