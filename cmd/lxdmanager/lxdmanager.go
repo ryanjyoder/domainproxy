e7e5709bdd7f56e1c3e4d0e0de7c3079aa0c6d77
Ryan J. Yoder <me@ryanjyoder.com>
2018-07-22 20:34:11 +0000

    simple stream

--- cmd/lxdmanager/lxdmanager.go
+++ cmd/lxdmanager/lxdmanager.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	lxd "github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/shared/api"
@@ -15,47 +16,36 @@ func launchContainer() error {
 	// Connect to LXD over the Unix socket
 	c, err := lxd.ConnectLXDUnix("/var/snap/lxd/common/lxd/unix.socket", nil)
 	if err != nil {
+		log.Println("couldnt connect to unix socket", err)
 		return err
-	}
-
-	// Container creation request
-	req := api.ContainersPost{
-		Name: "my-container",
-		Source: api.ContainerSource{
-			Type:  "image",
-			Alias: "my-image",
-		},
-	}
-
-	// Get LXD to create the container (background operation)
-	op, err := c.CreateContainer(req)
-	if err != nil {
-		return err
-	}
-
-	// Wait for the operation to complete
-	err = op.Wait()
-	if err != nil {
-		return err
-	}
-
-	// Get LXD to start the container (background operation)
-	reqState := api.ContainerStatePut{
-		Action:  "start",
-		Timeout: -1,
-	}
-
-	op, err = c.UpdateContainerState("my-container", reqState, "")
-	if err != nil {
-		return err
-	}
-
-	// Wait for the operation to complete
-	err = op.Wait()
-	if err != nil {
-		return err
-	}
-
-	return nil
-
+	// Connect to the remote SimpleStreams server
+d, err = lxd.ConnectSimpleStreams("https://images.linuxcontainers.org", nil)
+if err != nil {
+  return err
 }
+
+// Resolve the alias
+alias, _, err := d.GetImageAlias("centos/7")
+if err != nil {
+  return err
+}
+
+// Get the image information
+image, _, err := d.GetImage(alias.Target)
+if err != nil {
+  return err
+}
+
+// Ask LXD to copy the image from the remote server
+op, err := d.CopyImage(*image, c, nil)
+if err != nil {
+  return err
+}
+
+// And wait for it to finish
+err = op.Wait()
+if err != nil {
+  return err
+}
+
+}}
